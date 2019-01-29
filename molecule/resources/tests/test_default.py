ec4b3774e84d3c7fe50bea64cb3cf836ef04cbb1
Aliaksei <zelalexey@gmail.com>
2019-01-29 17:40:25 +0300

    added tests

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -12,3 +12,13 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_mysql_service(host):
+    service = host.service("mysql")
+    assert service.is_running
+    assert service.is_enabled
+
+
+def test_mysql_is_listening(host):
+    assert host.socket('tcp://127.0.0.1:3306').is_listening
