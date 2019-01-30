b7f824467feac04fd1f44f544bc78bff656daab7
Aliaksei <zelalexey@gmail.com>
2019-01-30 10:49:14 +0300

    lint fixes

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -5,11 +5,13 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+
 def test_hosts_file(host):
     f = host.file('/etc/hosts')
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
 
 def test_mysql_service(host):
     name = host.ansible("setup")["ansible_facts"]["mysql_service_name"]
@@ -17,6 +19,7 @@ def test_mysql_service(host):
         service = host.service(name)
         assert service.is_running
         assert service.is_enabled
+
 
 def test_mysql_is_listening(host):
     assert host.socket('tcp://127.0.0.1:3306').is_listening
